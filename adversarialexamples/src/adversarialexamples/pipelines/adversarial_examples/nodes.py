1cafdc6ccaf2750e320291c1972b2fd73d15932c
Jose Alejandro <jamontanac@unal.edu.co>
2024-02-11 18:36:47 -0500

    New requirements and new pipeline to generate adversarial examples

---
+++ adversarialexamples/src/adversarialexamples/pipelines/adversarial_examples/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'adversarial_examples'
+generated using Kedro 0.19.1
+"""
---
+++ adversarialexamples/src/adversarialexamples/pipelines/adversarial_examples/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'adversarial_examples'
+generated using Kedro 0.19.1
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
