66f8da1973bd9f76e0db94cd4a3cfbf5011a0615
Jose Alejandro <jamontanac@unal.edu.co>
2024-02-22 14:10:13 -0500

    moving all parameters to a globals so it is easier to change the configurations for all needed files

--- adversarialexamples/src/adversarialexamples/settings.py
+++ adversarialexamples/src/adversarialexamples/settings.py
@@ -32,10 +32,11 @@ CONFIG_LOADER_CLASS = OmegaConfigLoader
 CONFIG_LOADER_ARGS = {
       "base_env": "base",
       "default_run_env": "local",
-#       "config_patterns": {
+      "config_patterns": {
 #           "spark" : ["spark*/"],
-#           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
-#       }
+      "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
+      # "catalog": ["catalog*", "catalog*/**", "**/catalog*"],
+      }
 }
 
 # Class that manages Kedro's library components.
